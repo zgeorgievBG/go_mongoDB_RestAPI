f971ae5ed74fcce9ebc2ae89aeac84f5e13c236b
zgeorgievBG <79528041+zgeorgievBG@users.noreply.github.com>
2022-11-01 16:18:13 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import(
+ "net/http"
+ "github.com/julienschmidt/httprouter"
+ "gopkg.in/mgo.v2"
+ "github.com/zgeorgievBG/go_mongoDB_RestAPI"
+)
+
+func main() {
+
+r := httprouter.New()
+uc := controllers.NewUserController(getSession())
+r.GET("/user/:id", uc.GetUser )
+r.POST("/user", uc.CreateUser)
+r.DELETE("/user/:id", uc.DeleteUser)
+
+http.ListenAndServe("localhost:9000", r)
+
+}
+
+func getSession() *mgo.Session {
+
+	s, err :=mgo.Dial("mongodb://localhost:27017")
+
+	if err != nil {
+		panic(err)
+	}
+
+	return s
+}
